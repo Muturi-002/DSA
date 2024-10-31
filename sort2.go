8e3812efdddc3157a2d18dd0cc3a57bedf8ef3ff
Muturi-002 <martinmnjoroge03@gmail.com>
2024-10-31 15:34:40 +0300

    Successfully added mergesort algorithm to code. Need to include a goroutine to ensure that the output of the algorithm is as intended. If need be, use of waitgroups and locks to ensure the output is as intended.

--- sort2.go
+++ sort2.go
@@ -1,168 +1,112 @@
 package main
+
 import (
 	"fmt"
-	"time"
 	"math/rand"
+	"time"
 )
-func main() {
-	var sizeArray,slice []int
-	for j:=5;j<=30;j+=5{
-		sizeArray=append(sizeArray,j)
-	}
-	fmt.Println("Sizes for test: ",sizeArray)
-	for _, sizeArray:=range sizeArray {
-		fmt.Println("Size of slice: ",sizeArray)
-		for i:=0;i<sizeArray;i++{
-			randomNo:= rand.Intn(1000)
-			if !contains(slice,randomNo) {
-				slice=append(slice,randomNo)
-			}
-		}
-		fmt.Println("Slice: ", slice)
-		t:=time.Now()
-		slice=quick_sort(slice)
-		fmt.Println("Time taken: ",time.Since(t))
-		fmt.Println("Newly sorted slice: ",slice)
-		fmt.Println("==============")
-	}
-	
-}
 
-/* func merge_sort(slice1 []int)  []int{
-	length:=len(slice1)
-	return merge_sort_recursion(slice1,0,length-1)
-}
-func merge_sort_recursion(slice []int,l,r int) []int{
-	//var 'l' defines the first index of the slice; var 'r' defines the last index of the slice
-	if l<r{
-		midpoint:= (l +(r-l))/2
-		merge_sort_recursion(slice,l,midpoint)
-		merge_sort_recursion(slice,midpoint+1,r)
-		return merge_sorted_arrays(slice,l,midpoint,r)
-	}
-}
-func merge_sorted_arrays(slice []int,l,m,r int) []int{
-	//var 'm' is a placeholder for the midpoint
-	length_left:=len(slice[:m])
-	length_right:=len(slice[m:])
-	var left_slice []int
-	var right_slice []int
-	for p, _:=range slice[:length_left] {
-		left_slice = append(left_slice,p)
-	}
-	for q, _:=range slice[length_right:]{
-		right_slice = append(right_slice,q)
-	}
-	
-	var i,j,k int
-	for k=l;k<=r;k++{
-		i=0
-		j=0
-		for i<length_left && j<length_right{
-			if left_slice[i]<=right_slice[j]{
-				slice[k]=left_slice[i]
-				i++
-			}else{
-				slice[k]=right_slice[j]
-				j++
-			}
-		}
-		for i<length_left{
-			slice[k]=left_slice[i]
-			i++
-			k++
-		}
-		for j<length_right{
-			slice[k]=right_slice[j]
-			j++
-			k++
-		}
-	} */
-	// fmt.Println(left_slice)
-	// fmt.Println(right_slice)
-	/*for k=l;k<=r;k++{
-		for i=0{
-			for j=0{
-				if (i<length_left && j>=length_right) || left_slice[i]<=right_slice[j]{
-					slice[k]=left_slice[i]
-					i++
-				}else{
-					slice[k]=right_slice[j]
-					j++
-				}
-			}
-		}
-	}*/
-	// return slice
-// }
-
-/*func merge_sort(slice []int){
-	fmt.Println("Merge sort====")
-	fmt.Print("Slice to be sorted: ",slice,"\n")
-	if len(slice)==1{
-		return
-	}
-	k:=0
-	midLength:=len(slice)/2
-	left:=slice[:midLength]
-	right:=slice[midLength:]
-	merge_sort(left)
-	merge_sort(right)
-	left_index:= 0
-	right_index:= 0
-	for left_index< len(left) && right_index<len(right){
-		if left[left_index]<right[right_index]{
-			slice[k]=left[left_index]
-			left_index++
-		}else{
-			slice[k]=right[right_index]
-			right_index++
-		}
-		k++
-	}
-	fmt.Println("Newly sorted slice: ",slice)
-
-}*/
-func quick_sort(slice []int) []int{
-	if len(slice)< 2{
-		return slice
-	}
-
-	pivot:=slice[0]
-	var left_slice, right_slice []int
-
-	for i:=1;i<len(slice);i++{
-		if slice[i]<=pivot{
-			left_slice=append(left_slice,slice[i])
-		}else{
-			right_slice=append(right_slice,slice[i])
-		}
-	}
-	return append(append(quick_sort(left_slice),pivot),quick_sort(right_slice)...)
-}
-func quickSort(slice []int) []int {
+func quick_sort(slice []int) []int {
 	if len(slice) < 2 {
 		return slice
 	}
 
 	pivot := slice[0]
-	var leftSlice, rightSlice []int
+	var left_slice, right_slice []int
 
 	for i := 1; i < len(slice); i++ {
 		if slice[i] <= pivot {
-			leftSlice = append(leftSlice, slice[i])
+			left_slice = append(left_slice, slice[i])
 		} else {
-			rightSlice = append(rightSlice, slice[i])
+			right_slice = append(right_slice, slice[i])
 		}
 	}
-
-	return append(append(quickSort(leftSlice), pivot), quickSort(rightSlice)...)
+	return append(append(quick_sort(left_slice), pivot), quick_sort(right_slice)...)
 }
-func contains(slice []int, numbers int)bool{
-	for _,v:=range slice{
-		if v==numbers{
+
+func contains(slice []int, numbers int) bool { //serves both quick_sort and merge_sort functions
+	for _, v := range slice {
+		if v == numbers {
 			return true
 		}
 	}
 	return false
+}
+func mergesort(slice []int) []int{
+	l,r:= 0,len(slice)-1
+	if len(slice)==1{
+		fmt.Println("Already sorted")
+		return slice
+	}
+	mid:= (l+r)/2
+	left_slice:=slice[:mid]
+	right_slice:=slice[mid:]
+	
+	return merge(left_slice,right_slice)
+}
+func merge(left,right []int) []int{
+	left_index, right_index:= 0,0 //mark the first index of the prototype slices respectively
+	newSlice:= make([]int, len(left)+len(right))//create a new  sorted slice
+	for left_index<len(left) && right_index<len(right){
+		if left[left_index]<=right[right_index]{
+			newSlice= append(newSlice, left[left_index])
+			left_index++
+		}else{
+			newSlice= append(newSlice, right[right_index])
+			right_index++
+		}
+	}
+
+	return newSlice
+}
+func main() {
+	var sizeArray, slice []int
+	for j := 5; j <= 30; j += 5 {
+		sizeArray = append(sizeArray, j)
+	}
+	var choice int
+	fmt.Println("Sizes for test: ", sizeArray)
+	fmt.Println("Choose a sorting algorithm:")
+	fmt.Println("\t1. Quicksort algorithm\n\t2. Mergesort algorithm")
+	fmt.Scanln(&choice)
+	switch choice {
+	case 1:
+		{
+			for _, sizeArray := range sizeArray {
+				fmt.Println("Size of slice: ", sizeArray)
+				for i := 0; i < sizeArray; i++ {
+					randomNo := rand.Intn(1000)
+					if !contains(slice, randomNo) {
+						slice = append(slice, randomNo)
+					}
+				}
+				fmt.Println("Slice: ", slice)
+				t := time.Now()
+				slice = quick_sort(slice)
+				fmt.Println("Time taken: ", time.Since(t))
+				fmt.Println("Newly sorted slice: ", slice)
+				fmt.Println("==============")
+			}
+		}
+		break
+	case 2:
+		for _, sizeArray := range sizeArray {
+			fmt.Println("Size of slice: ", sizeArray)
+			for i := 0; i < sizeArray; i++ {
+				randomNo := rand.Intn(1000)
+				if !contains(slice, randomNo) {
+					slice = append(slice, randomNo)
+				}
+			}
+			fmt.Println("Slice: ", slice)
+			t := time.Now()
+			slice = mergesort(slice)
+			fmt.Println("Time taken: ", time.Since(t))
+			fmt.Println("Newly sorted slice: ", slice)
+			fmt.Println("==============")
+		}
+		break
+	default:
+		break
+	}
 }
