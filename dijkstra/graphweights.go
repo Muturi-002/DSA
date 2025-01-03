d5380277ede12b14fde90bed90d9278a72a0ed3a
Muturi-002 <martinmnjoroge03@gmail.com>
2025-01-04 00:31:27 +0300

    Unsuccessfully created a functioning algorithm. Need to find the bug, certain it is in the Dijkstra method.

--- dijkstra/graphweights.go
+++ dijkstra/graphweights.go
@@ -1,10 +1,14 @@
 /*This is a copy of weightedGraphs.go in the root directory. It is specific to the dijkstra directory, meant to be imported to the dijkstra.go file.
-*/
+ */
 package main
+
 import (
-	"fmt"
 	"errors"
+	"fmt"
+	//d "github.com/Muturi-002/DSA/dijkstra/d2"
 )
+const infinity int= 99999
+var vertCount int//number of vertices in the graph
 type Graph struct {
 	vertices []*Vertex
 	edges []*Edge
@@ -17,15 +21,15 @@ type Vertex struct {
 type Edge struct {
 	weight int//weight of an edge
 }
-func (g *Graph) AddVertex(k string) {
-	if contains(g.vertices, k) {
+func (g *Graph) addVertex(k string) {
+	if Contains(g.vertices, k) {
 		err := fmt.Errorf("vertex %s not added because it already exists", k)
 		fmt.Println(err.Error())
 	} else {
 		g.vertices = append(g.vertices, &Vertex{key: k})
 	}
 }
-func contains(s []*Vertex, k string) bool {
+func Contains(s []*Vertex, k string) bool {
 	for _,v:=range s {
 		if k==v.key {
 			return true
@@ -33,9 +37,9 @@ func contains(s []*Vertex, k string) bool {
 	}
 	return false
 }
-func (g *Graph) AddEdge(from, to string, distance int) { // the variables represent the root and destination vertices respectively
-	fromVertex := g.getVertex(from)
-	toVertex := g.getVertex(to)
+func (g *Graph) addEdge(from, to string, distance int) { // the variables represent the root and destination vertices respectively
+	fromVertex := g.GetVertex(from)
+	toVertex := g.GetVertex(to)
 	defer func() {
 		if r := recover(); r != nil {
 			fmt.Println("Recovered from error:", r)
@@ -50,7 +54,7 @@ func (g *Graph) AddEdge(from, to string, distance int) { // the variables repres
 			fmt.Print("Vertex ", to, " does not exist\n")
 		}
 		panic(err)
-	} else if contains(fromVertex.neighbours, to) { // checks if there is an existing edge between the two vertices
+	} else if Contains(fromVertex.neighbours, to) { // checks if there is an existing edge between the two vertices
 		err := errors.New("'Edge already exists'")
 		fmt.Print(err, from, "-->", to, "\n")
 		panic(err)
@@ -63,7 +67,7 @@ func (g *Graph) AddEdge(from, to string, distance int) { // the variables repres
 	g.edges = append(g.edges, &edge)//dereference the Edge{} pointer
 	fmt.Printf("Edge added: %s --(%d)--> %s\n", from, edge.weight, to)
 }
-func (g *Graph) getVertex(k string) *Vertex { //checks for a vertex in the graph
+func (g *Graph) GetVertex(k string) *Vertex { //checks for a vertex in the graph
 	for i, v:=range g.vertices{
 		if v.key==k{
 			return g.vertices[i]
@@ -71,7 +75,7 @@ func (g *Graph) getVertex(k string) *Vertex { //checks for a vertex in the graph
 	}
 	return nil
 }
-func (g Graph) Print() {
+func (g Graph) print() {
 	// print out each vertex and its neighbours with weights
 	for _, v := range g.vertices {
 		fmt.Print("Vertex ", v.key, ": ")
@@ -85,22 +89,77 @@ func (g Graph) Print() {
 	}
 	fmt.Println()
 }
+func Dijkstra(g *Graph,startkey string) {
+	fmt.Println("====Dijkstra's Algorithm====")
+	distance := make([]int, len(g.vertices))//creates an array for the shortest distance from the source vertex to the neighbouring vertices
+    visited := make([]bool, len(g.vertices))
+	vertCount= len(g.vertices)
+	for k := 0; k < vertCount; k++ {
+        distance[k] = infinity
+        visited[k] = false
+    }
 
+	start := g.GetVertex(startkey)
+	if start == nil {
+		fmt.Println("Vertex not found")
+		return
+	}
+	var startIndex int//enables the distance between the start vertex and itself to be zero. Setting the distance index as start will create an error.
+	for i, v := range g.vertices {
+		if v == start {
+			startIndex = i
+			break
+		}
+	}
+	distance[startIndex] = 0
 
+    for k := 0; k < vertCount; k++ {
+        m := miniDist(distance, visited)
+        visited[m] = true
+
+        for l := 0; l < vertCount; l++ {
+			for _, edge := range g.vertices[m].w {
+				//neighbour := g.vertices[m].neighbours[k]
+				if !visited[l] && edge.weight != 0 && distance[m] != infinity &&
+					distance[m]+edge.weight < distance[l] {
+					distance[k] = distance[m] + edge.weight
+				}
+			}
+            }
+        }
+    fmt.Println("Vertex\t\tDistance from source vertex")
+    for k := 0; k < len(g.vertices); k++ {
+        fmt.Printf("%s\t\t\t%d\n", g.vertices[k], distance[k])
+    }
+}
+func miniDist(distance []int, visited []bool) int {// a method that seeks to find the smallest distance from the source vertex to the neighbouring vertices
+    minimum := infinity
+    var ind int// stores the index of the vertex with the smallest distance
+    for k := 0; k < 6; k++ {
+        if !visited[k] && distance[k] < minimum {
+            minimum = distance[k]
+            ind = k
+        }
+    }
+    return ind
+}
 func main(){
-	defer fmt.Println("---Weighted Graphs---")
+	fmt.Println("---Weighted Graphs---")
 	g := Graph{}
 	vertices:=[]string{"A","B","C","D","E"}
 	for _,r:=range vertices{
-		g.AddVertex(r)
+		g.addVertex(r)
 	}
 	fmt.Println("Vertices of graph:")
-	fmt.Println(g)
-	g.Print()
-	g.AddEdge("A", "B", 5)
-	g.AddEdge("A", "C", 10)
-	g.AddEdge("B", "C", 6)
-	g.AddEdge("C", "D", 15)
-	g.AddEdge("D", "E", 20)
-	g.Print()
+	g.addEdge("A", "B", 5)
+	g.addEdge("A", "C", 10)
+	g.addEdge("B", "C", 6)
+	g.addEdge("C", "D", 15)
+	g.addEdge("D", "E", 20)
+	g.print()
+	var src string
+	fmt.Printf("Choose the start vertex: ")
+	fmt.Scanf("%s", &src)
+	fmt.Println("\nShortest path from ", src, " to other vertices:")
+	Dijkstra(&g, src)
 }
